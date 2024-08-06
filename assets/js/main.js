da3b4a8f4d03309fc2f4f44290a330f23922a5a6
Muhammad Iswarol Kiylacs <iswarol17@gmail.com>
2024-08-06 23:30:44 +0700

    The Skills, Project, and Contact sections have been completed

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+/*=============== SERVICES MODAL ===============*/
+
+/*=============== MIXITUP FILTER PROJECTS ===============*/
+let mixerProjects = mixitup(".work__container", {
+  selectors: {
+    target: ".work__card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
+
+/* Link active work */
+const linkWork = document.querySelectorAll(".work__item");
+
+function activeWork() {
+  linkWork.forEach((l) => l.classList.remove("active-work"));
+  this.classList.add("active-work");
+}
+
+linkWork.forEach((l) => l.addEventListener("click", activeWork));
+
+/*=============== SWIPER CONTENT ===============*/
+let swiperContent = new Swiper(".content__container", {
+  spaceBetween: 30,
+  loop: true,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+  },
+});
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== LIGHT DARK THEME ===============*/
+
+/*=============== SCROLLREVEAL ANIMATION ===============*/
